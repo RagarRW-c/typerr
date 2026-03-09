318ecb2f8ca1609ed783d11050ae5cf6d318d991
Witalij <witalij.rapicki@gmail.com>
2026-03-09 15:11:07 +0100

    feat: iam policy role added in ecs

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -41,6 +41,27 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
+resource "aws_iam_role_policy" "github_actions_passrole" {
+  name = "${var.project_name}-github-passrole"
+  role = "github-actions-typrr"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = [
+          aws_iam_role.ecs_task_execution.arn,
+          aws_iam_role.ecs_task.arn
+        ]
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
